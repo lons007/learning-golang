b03dc290034115fe090f09c2a1178db586f721ca
Nikolay V <nikolayv0212@gmail.com>
2026-01-31 14:37:55 -0800

    fix slice

--- code-traning/slice/slice.go
+++ code-traning/slice/slice.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 )
 
-func main(){
-	numbers := make([]float64, 3)
-	numbers[0] =19.7
-	numbers[2] = 25.2
-	for i, number := range numbers {
-		fmt.Println(i, number)
-	}
+// func main(){
+// 	numbers := make([]float64, 3)
+// 	numbers[0] =19.7
+// 	numbers[2] = 25.2
+// 	for i, number := range numbers {
+// 		fmt.Println(i, number)
+// 	}
 
-	var letters = []string{"a", "b", "c"}
-	for i, letter := range letters {
-		fmt.Println(i, letter)
+// 	var letters = []string{"a", "b", "c"}
+// 	for i, letter := range letters {
+// 		fmt.Println(i, letter)
+// 	}
+// }
+
+func main() {
+	array := [5]string{"a", "b", "c", "d", "f"}
+	slice := array[1:3]
+	// slice = append(slice, "x")
+	// slice = append(slice, "y", "z")
+	for _, letter := range slice {
+		fmt.Println(letter)
 	}
 }
