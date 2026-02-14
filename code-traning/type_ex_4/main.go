b4f22892f18b0b2487e7e8878e569f4ae88b41cb
Nikolay V <nikolayv0212@gmail.com>
2026-02-14 14:56:36 -0800

    type and methods

---
+++ code-traning/type_ex_4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+type Number int
+
+func (n *Number) Display() {
+	fmt.Println(*n)
+}
+
+func (n *Number) Double() {
+	*n *= 2
+}
+
+
+func main() {
+	number := Number(4)
+	number.Double()
+	number.Display()
+
+	// fmt.Println("Original value of number:", number)
+	// fmt.Println("number after calling Double:", number)
+}
