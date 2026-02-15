389b75487e35dda58b213b9f0397eeaf3f4093b6
Nikolay V <nikolayv0212@gmail.com>
2026-02-15 09:50:14 -0800

    type and methods

--- code-traning/type_ex_4/main.go
+++ code-traning/type_ex_4/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Number int
+type DoubleNumbers int
 
 func (n *Number) Display() {
 	fmt.Println(*n)
@@ -15,10 +16,17 @@ func (n *Number) Double() {
 	*n *= 2
 }
 
+func (n *Number) DoubleNumbers() DoubleNumbers{
+	return DoubleNumbers(*n * 2)
+}
 
 func main() {
 	number := Number(4)
+	number.Display()
 	number.Double()
+	number.Display()
+	fmt.Println(number.DoubleNumbers())
+	// number.DoubleNumbers()
 	number.Display()
 
 	// fmt.Println("Original value of number:", number)
